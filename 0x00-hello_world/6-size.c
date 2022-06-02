5f45e2afad23161cdc2112ed4cae8eea24c2953b
Jamesxx98 <Jamesmaina445@gmail.com>
2022-06-02 15:19:37 -0700

    first c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Prints "Programming is like building a multilingual
+*                puzzle, followed by a new line."
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+
+	return (0);
+}
