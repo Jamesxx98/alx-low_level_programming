9e8f019101a7b9c6f2fddd5e31197fece5b21e42
Jamesxx98 <Jamesmaina445@gmail.com>
2022-06-07 01:52:53 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "alx.h"
+
+/**
+ * main - program that prints alx.
+ * Return: int
+ */
+
+int main(void)
+{
+int n = 0;
+char str_alx[4] = "alx";
+while (n < 4)
+{
+_putchar(str_alx[n]);
+n += 1;
+}
+_putchar('\n');
+	return (0);
+}
